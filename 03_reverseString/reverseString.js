82c6a2e746660233b2f234ddb4ea835279275539
Valentin Crogiez <valentin.crogiez@gmail.com>
2022-10-12 11:33:03 +0200

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = [];
+    for (char of string) {
+        array.push(char);
+    }
+    array = array.reverse();
+    let result = "";
+    for (char of array) {
+        result += char;
+    }
+    return result;
 };
 
 // Do not edit below this line
