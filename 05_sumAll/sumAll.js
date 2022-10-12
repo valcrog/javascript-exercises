35098c4450f18dc719b943b62d07c562a10a038a
Valentin Crogiez <valentin.crogiez@gmail.com>
2022-10-12 17:49:42 +0200

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
+    else if (a > b) {
+        [a, b] = [b, a];
+    }
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
