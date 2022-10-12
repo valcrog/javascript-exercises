719b5ad1f459b273ccfc712c9cbd64c5e82e3a1b
Valentin Crogiez <valentin.crogiez@gmail.com>
2022-10-12 11:38:23 +0200

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    for (element of elements) {
+        for (let i = array.length-1;i>=0;i--) {
+            if (array[i] === element) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
