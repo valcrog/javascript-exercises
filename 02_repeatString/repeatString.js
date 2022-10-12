4dee69ba2398b2bf867893c0fcae2a6d9314bf5e
Valentin Crogiez <valentin.crogiez@gmail.com>
2022-10-12 11:29:54 +0200

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    for (let i=0; i<num; i++) {
+        result += string;
+    }
+    if (num < 0) {
+        result = "ERROR";
+    }
+    return result;
 };
 
 // Do not edit below this line
