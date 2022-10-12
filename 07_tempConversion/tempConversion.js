97c1af8a0486a62d5c6612460077b6f6a050a8f3
Valentin Crogiez <valentin.crogiez@gmail.com>
2022-10-12 18:00:24 +0200

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp - 32) * (5/9);
+  return (Math.round(temp * 10)/10);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = temp * (9/5) + 32;
+  return (Math.round(temp * 10)/10);
 };
 
 // Do not edit below this line
