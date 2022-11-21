8f1e6e6c662598032f62a46b14f9c2ddcb07f1f9
Valentin Crogiez <valentin.crogiez@gmail.com>
2022-11-21 17:19:22 +0100

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n <= 0) {
+        return "OOPS"
+    }
+    else if (n == 1) {
+        return 1
+    } else {
+        let n1 = 0;
+        let n2 = 1;
+        let nextTerm;
+        for (let i = 1; i < n; i++) {
+            nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
+        }
+        return nextTerm
+    }
 };
 
 // Do not edit below this line
