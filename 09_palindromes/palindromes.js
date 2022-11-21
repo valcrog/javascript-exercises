e74ec6b5556dc0c56c6da30f9f5d26074c1a529c
Valentin Crogiez <valentin.crogiez@gmail.com>
2022-11-21 17:12:54 +0100

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let stripped = s.replace(/[^\w\s\']|_/g, "")
+                    .replace(/\s+/g, "");
+    stripped = stripped.toLowerCase();
+    return stripped == stripped.split("").reverse().join("")
 };
 
 // Do not edit below this line
