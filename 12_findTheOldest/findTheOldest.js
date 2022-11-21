e14c65182a8680769911c9b4df3516a87c9cdffd
Valentin Crogiez <valentin.crogiez@gmail.com>
2022-11-21 17:42:23 +0100

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peoples) {
+    peoples.forEach(function(people) {
+        if (!("yearOfDeath" in people)) {
+            people.yearOfDeath = (new Date()).getFullYear()
+        }
+    })
+    const oldest = peoples.sort(function(a, b) {
+        lastPerson = a.yearOfDeath - a.yearOfBirth;
+        nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1: 1;
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
